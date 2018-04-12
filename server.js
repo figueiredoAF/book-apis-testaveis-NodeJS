6cda1774d2247e18f803a9ed291597fa44e160aa
Alex Fernandes Figueiredo <alex.figueiredo@nutes.uepb.edu.br>
2018-04-12 18:36:24 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    res.send('Hello world!');
+});
+
+app.listen(3000, ()=>{
+    console.log('Example app listening on port 3000!');
+});
